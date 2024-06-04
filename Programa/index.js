d815f413ca0043a9a4d3a936b87046af00dcd56f
Jacob Diaz <jacobdiaz22584741@gmail.com>
2024-06-04 12:44:06 -0600

    Agregar codigo en el archivo index.js

---
+++ Programa/index.js
@@ -0,0 +1,31 @@
+const hourEl = document.getElementById("horas");
+const minutesEl = document.getElementById("minutos");
+const secondEl = document.getElementById("segundos");
+const ampmEl = document.getElementById("ampm");
+
+function  updateClock(){
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    let ampm = "AM"
+
+    if(h>12){
+        h = h-12
+        ampm = "PM"
+    }
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    hourEl.innerText = h;
+    minutesEl.innerText = m;
+    secondEl.innerText = s;
+    ampmEl,(innerText =  ampm);
+
+    setTimeout(() => {
+        updateClock()
+    }, 1000)
+} 
+
+updateClock();
